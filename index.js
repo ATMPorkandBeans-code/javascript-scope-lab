f31d3360d07eba05f1044dc1cc5468dd42a9450b
Adrian Moring <adriantmoringbass@gmail.com>
2025-08-09 16:34:59 -0400

    first iteration

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake"
+
+function addBurger(){
+    const newBurger = "FlatBurger";
+    burgers.push(newBurger)
+ 
+    if (burgers){
+        const anothernewBurger = "Maple Bacon Burger";
+        burgers.push(anothernewBurger)
+        console.log(burgers)
+    }
+};
+
+changeFeaturedDrink = () => {
+    featuredDrink = "The JavaShake"
+    console.log(featuredDrink)
+};
+
+addBurger();
+changeFeaturedDrink();
+
+
